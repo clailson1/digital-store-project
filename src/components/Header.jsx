028580aaf2eb3ff6934281d165fc4c4c4563a731
Clailson <gadelhaclailson@gmail.com>
2025-05-07 19:55:38 -0300

    style(header): aplicar flexbox e estilos
    Estilização do Header utilizando flexbox, Criação da pasta styles e do arquivo Header.css. importaçãodo PrimeReact no Arquivo main.jsx.
    
    📦 Pacotes:
    - Instalação e importação do PrimeReact no main.jsx
    
    🛠 Arquivos modificados:
    -main.jsx: importação do PrimeReact
    -components/Header.jsx: aplicação de estilização com Flexbox
    -styles/Header.css: criado para conter os estilos do Header
    -Criada a pasta styles/ para organizar os arquivos CSS
    
    🧪 Testado em:
    - Navegador (Chrome)
    
    📌 Observações:
    - O Header está sem responsividade, pois eu não usei o Menubar do PrimeReact

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,47 @@
+import Logo from "./Logo";
+import "../styles/Header.css"
+import miniCart from '../assets/mini-cart.svg';
+
+
 const Header = () => {
     return ( 
-        <>
-            <h1 className="bg-blue-200 text-red-600">Digital Store</h1>
-        </>
+        <div className="header-div">
+            <div className="flex">
+                <Logo />
+            </div>
+            <div id="search-bar" className="search-container">
+                <input 
+                    type="text" 
+                    id="search" 
+                    name="search"
+                    placeholder="Pesquisar Produto..."
+                    className="search-input"
+                />
+                <i 
+                    className="pi pi-search search-icon" 
+                />
+            </div>
+            <div className="register-login">
+                <a 
+                    href=""
+                    className="register-link"
+                >
+                    Cadastre-se
+                </a>
+                <div className="login-button">
+                <a 
+                    href=""
+                    className="login-link"
+                >
+                    Entrar
+                </a>
+            </div>
+            </div>
+            
+            <div>
+                <img src={miniCart} alt="" />
+            </div>
+        </div>
     );
 }
  
