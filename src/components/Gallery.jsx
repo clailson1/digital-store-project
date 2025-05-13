9451babb1816dbfa5e6a4a0a043d38bd2ab583da
Clailson <gadelhaclailson@gmail.com>
2025-05-13 12:03:32 -0300

    feat(gallery): adiciona componente Gallery e integração na página Home
    📦 Pacotes:
    - Nenhum
    
    🛠 Arquivos modificados:
    - src/components/Gallery.jsx (novo)
    - src/styles/Gallery.css (novo)
    - src/data/gallery.js (novo)
    - src/assets/arrow-left.svg (novo)
    - src/assets/arrow-right.svg (novo)
    - src/assets/star-icon.svg (novo)
    - src/components/Home.jsx (modificado)
    
    🧪 Testado em:
    - Navegador local com renderização da galeria na Home
    
    📌 Observações:
    - A galeria foi criada com ícones de seta, estilizada via CSS e conectada à página Home.

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,29 @@
+import "../styles/Gallery.css";
+
+const Gallery = ({ width, height, radius, showThumbs, images }) => {
+
+    return (  
+        <div className="slide">
+            <div className="slide-image">
+            <img src={images.src} alt="" width={width} height={height} style={{ borderRadius: radius}}/>
+            </div>
+            {/* {
+                if (showThumbs) {
+                    return (
+                        <div>
+                            <h3>thumbs</h3>
+                        </div>
+                    );
+                }
+            } */}
+            {
+                showThumbs && 
+                <div className="thumbs">
+                    <h3>thumbs</h3>
+                </div>
+            }
+        </div>
+    );
+}
+ 
+export default Gallery
