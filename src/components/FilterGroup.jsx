0e6e405c4c1920dff88bfd1ceb14836389f938d8
Clailson <gadelhaclailson@gmail.com>
2025-05-14 20:17:51 -0300

    style(ProductListing): ajusta estilos da página de produtos
    - Adiciona estilos para o componente FilterGroup
    - Atualiza estilos da ProductListingPage
    - Cria novo arquivo FilterGroup.css
    
    modified: src/components/FilterGroup.jsx
    modified: src/pages/ProductListingPage.jsx
    new file: src/styles/FilterGroup.css
    modified: src/styles/ProductListingPage.css

--- src/components/FilterGroup.jsx
+++ src/components/FilterGroup.jsx
@@ -1,9 +1,11 @@
+import "../styles/FilterGroup.css"
+
 const FilterGroup = ({title, inputType, text, values}) => {
     return (  
-        <div>
+        <div className="filter-group">
             <h3>{ title }</h3>
             <div>
-                <input type={inputType} id="vehicle1" name="vehicle1" value={values}></input>
+                <input className={"custom-checkbox"} type={inputType} id="vehicle1" name="vehicle1" value={values}></input>
                 <label htmlFor="vehicle1">{ text }</label>
             </div>   
         </div>  
